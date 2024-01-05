4b96623414ff6d363d7e9f8722d898294ff29e04
OlegM <olegm.ua@gmail.com>
2024-01-06 01:04:18 +0200

    Added dev

--- src/config/settings/base_settings.py
+++ src/config/settings/base_settings.py
@@ -70,17 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -111,11 +100,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- src/config/settings/stage.py
+++ src/config/settings/stage.py
@@ -2,6 +2,7 @@ from config.settings.base_settings import *  # NOQA
 
 SECRET_KEY = "django-insecure-idv$okbx1eu)g+f#_v@_)m=m+g=s49jt=+hq)bvf2etspmkg$5"
 DEBUG = False
+
 ALLOWED_HOSTS = []
 
 DATABASES = {
